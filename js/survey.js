a39218e9079166e054796e7518f8e3785f0762ff
Caroline <cal31391@icloud.com>
2017-10-23 13:29:59 -0400

    ugh

--- js/survey.js
+++ js/survey.js
@@ -1,10 +1,12 @@
 function changePage() {
     var radios = document.querySelectorAll('input[type="radio"]:checked');
+    var radio_len = radios.length;
+
     if (radios.length < 7) {
-        createSummaryPage();
+        createSummaryPage(radio_len);
         //alert('Please select an option for every question.');
     } else {
-        createSummaryPage();
+        createSummaryPage(radio_len);
     }
 } // https://stackoverflow.com/questions/1423777/
 // how-can-i-check-whether-a-radio-button-is-selected-with-javascript
@@ -15,11 +17,18 @@ function checkUserInput() {
     //all inputs are valid, i.e - an actual character string
 }
 
-function createSummaryPage() {
+function createSummaryPage(radio_length) {
 
     var main_div = document.getElementById("main");
     var content_div = document.getElementById("list");
     var user_name = document.getElementById("usr").value;
+    var thank_you_msg = "Thank you for taking our survey, " + user_name + "!"
+    var radio_len = radio_length;
+
+    var rating_form;
+    var rating;
+    var element_id;
+
     document.getElementById("name-row").innerHTML = "";
 
     main_div.innerHTML =
@@ -27,23 +36,26 @@ function createSummaryPage() {
         '<h1>Survey Summary</h1>\n' +
         '</div>\n';
 
-    content_div.innerHTML = "Thank you for taking our survey, " + user_name + "!";
-    //       ^       ^      ^      ^
-    // call calculateSurveyNums() here to create div
+    content_div.innerHTML = thank_you_msg;
 
-    document.appendChild(main_div);
-    document.appendChild(content_div);
-}
 
-function calculateSurveyNums() {
-    //calculate radio values for each question
-    //create new div element for content
-    var rating = document.querySelector('input[name="rating1"]:checked').value;
+    for (var i = 0; i < radio_len; i++) {
 
-    //put for loop here:
-    //dynamically create summary for all 7 questions
-    // for radios:checked in radios, iterate and fill rating variable with value for current element in loop, 
-    // add to list, keep going to next until all are processed, put in div
+        element_id = "q" + i;
 
-    //return div element
+        rating_form = document.getElementById(element_id);
+
+        rating_set = document.getElementsByName("rating1");
+        for (var i = 0; i < rating_set.length; i++) {
+            if (rating_set[i].checked) {
+                rating = rating_set[i].value;
+            }
+        }
+
+
+
+        //rating_form.innerHTML = rating;
+
+    }
+    alert(i);
 }
